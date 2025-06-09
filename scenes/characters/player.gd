2f9aee3ca40035ead23d5713411c6e5f4af512f9
vpinti <vittorio.pinti@gmail.com>
2025-06-09 19:53:12 +0200

    added node-based state machine

--- scenes/characters/player.gd
+++ scenes/characters/player.gd
@@ -2,6 +2,7 @@ class_name Player
 extends CharacterBody2D
 
 enum ControlScheme {CPU, P1, P2}
+enum State {MOVING, TACKLING}
 
 @export var control_scheme: ControlScheme
 @export var speed: float
@@ -9,22 +10,25 @@ enum ControlScheme {CPU, P1, P2}
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var player_sprite: Sprite2D = %PlayerSprite
 
+var current_state: PlayerState = null
 var heading := Vector2.RIGHT
+var state_factory := PlayerStateFactory.new()
+
+func _ready() -> void:
+	switch_state(State.MOVING)
 
 func _process(_delta: float) -> void:
-	if control_scheme == ControlScheme.CPU:
-		pass # processs aI movement
-	else:
-		handle_human_movement()
-	
-	set_movement_animation()
-	set_heading()
 	flip_sprites()
 	move_and_slide()
 
-func handle_human_movement() -> void:
-	var direction := KeyUtils.get_input_vector(control_scheme)
-	velocity = direction * speed
+func switch_state(state: State) -> void:
+	if current_state != null:
+		current_state.queue_free()
+	current_state = state_factory.get_fresh_state(state)
+	current_state.setup(self, animation_player)
+	current_state.state_transition_requested.connect(switch_state.bind())
+	current_state.name = "PlayerStateMachine: " + str(state)
+	call_deferred("add_child", current_state)
 	
 func set_movement_animation() -> void:
 	if velocity.length() > 0:
